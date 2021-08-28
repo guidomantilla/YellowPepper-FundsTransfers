84f60576c1a8ab5fa0df2e3771aca146a274d623
guido.mantilla <gxm39v@amount.com>
2021-08-27 20:11:38 -0500

    refactor

--- pkg/core/service/dto/Transfer.go
+++ pkg/core/service/dto/Transfer.go
@@ -8,6 +8,6 @@ type Transfer struct {
 	OriginAccount      int64               `json:"origin_account,omitempty"`
 	DestinationAccount int64               `json:"destination_account,omitempty"`
 	Amount             float64             `json:"amount,omitempty"`
-	Status string              `json:"status,omitempty"`
-	Errors exception.Exception `json:"errors,omitempty"`
+	Status             string              `json:"status,omitempty"`
+	Errors             exception.Exception `json:"errors,omitempty"`
 }
