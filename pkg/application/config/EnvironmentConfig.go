559d85d8223dfb97beb15b0663c9c644d7a5b39b
guido.mantilla <gxm39v@amount.com>
2021-09-04 13:02:18 -0500

    refactor

--- pkg/application/config/EnvironmentConfig.go
+++ pkg/application/config/EnvironmentConfig.go
@@ -2,13 +2,12 @@ package config
 
 import (
 	"YellowPepper-FundsTransfers/pkg/misc/environment"
+	"YellowPepper-FundsTransfers/pkg/misc/files"
 	"YellowPepper-FundsTransfers/pkg/misc/properties"
-	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 const (
@@ -31,10 +30,10 @@ func LoadEnvironment() environment.Environment {
 	resourcesFolder := retrieveResourcesFolder(env)
 	profile := env.GetValueOrDefault(PROFILE, PROFILE_DEFAULT_VALUE).AsString()
 
-	mainFileArgs := readFile(filepath.Join(resourcesFolder, "application.properties"))
+	mainFileArgs := files.ReadFile(filepath.Join(resourcesFolder, "application.properties"))
 	mainFileSource := properties.NewDefaultPropertySource(MAIN_FILE_PROPERTY_SOURCE_NAME, properties.NewDefaultProperties().FromArray(mainFileArgs).Build())
 
-	profileFileArgs := readFile(filepath.Join(resourcesFolder, "application-"+profile+".properties"))
+	profileFileArgs := files.ReadFile(filepath.Join(resourcesFolder, "application-"+profile+".properties"))
 	profileFileSource := properties.NewDefaultPropertySource(PROFILE_FILE_PROPERTY_SOURCE_NAME, properties.NewDefaultProperties().FromArray(profileFileArgs).Build())
 
 	env.AppendPropertySources(mainFileSource, profileFileSource)
@@ -42,48 +41,14 @@ func LoadEnvironment() environment.Environment {
 	return env
 }
 
-func readFile(fullFileName string) *[]string {
-
-	lines := make([]string, 0)
-
-	file, err := os.Open(fullFileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(file)
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, strings.TrimSpace(scanner.Text()))
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	return &lines
-}
-
 func retrieveResourcesFolder(env environment.Environment) string {
 	workingDirectory, _ := os.Getwd()
 	sourceFolderName := env.GetValueOrDefault(SOURCE_FOLDER_NAME, SOURCE_FOLDER_NAME_DEFAULT_VALUE).AsString()
 	sourceRootDirectory := filepath.Join(workingDirectory, sourceFolderName)
-	if !validateIfFolderExists(sourceRootDirectory) {
+	if !files.ValidateIfFolderExists(sourceRootDirectory) {
 		sourceRootDirectory = filepath.Join(workingDirectory)
 	}
 	log.Println(fmt.Sprintf("Configured Resources Folder: %s", sourceRootDirectory))
 	return filepath.Join(sourceRootDirectory, ".resources")
 }
 
-func validateIfFolderExists(directory string) bool {
-	_, err := os.Stat(directory)
-	if err != nil {
-		return false
-	}
-	return true
-}
